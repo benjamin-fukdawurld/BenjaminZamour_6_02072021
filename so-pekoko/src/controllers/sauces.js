dae6d5966aca5ceada652643bccebda21d359502
benjamin <benjamin.zamour@gmail.com>
2021-07-03 16:04:11 +0200

    controller/sauces.js: Add sauces controller skeleton

---
+++ so-pekoko/src/controllers/sauces.js
@@ -0,0 +1,29 @@
+class Controller {
+  getSauces(req, res) {
+    res.status(500).send({});
+  }
+
+  getSauce(req, res) {
+    res.status(500).send({});
+  }
+
+  addSauce(req, res) {
+    res.status(500).send({});
+  }
+
+  updateSauce(req, res) {
+    res.status(500).send({});
+  }
+
+  deleteSauce(req, res) {
+    res.status(500).send({});
+  }
+
+  likeSauce(req, res) {
+    res.status(500).send({});
+  }
+}
+
+const controller = new Controller();
+
+export default controller;
