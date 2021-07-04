68468e60c9714b360a5c5c328705f77a24fc716d
benjamin <benjamin.zamour@gmail.com>
2021-07-04 14:16:17 +0200

    routes/auth.js: Add users and delete route

--- so-pekoko/src/routes/auth.js
+++ so-pekoko/src/routes/auth.js
@@ -3,8 +3,12 @@ import controller from '../controllers/auth.js';
 
 const router = express.Router();
 
+router.get('/api/auth/users', controller.getUsers.bind(controller));
+
 router.post('/api/auth/signup', controller.signup.bind(controller));
 
 router.post('/api/auth/login', controller.login.bind(controller));
+
+router.delete('/api/auth/delete', controller.deleteUser.bind(controller));
 
 export default router;
