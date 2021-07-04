604840d8a9d392b263e530e043af1df0af3ded87
benjamin <benjamin.zamour@gmail.com>
2021-07-04 20:36:23 +0200

    routes/auth.js: Shortened routes

--- so-pekoko/src/app.js
+++ so-pekoko/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(auth);
+app.use('/api/auth', auth);
 app.use(sauces);
 
 export default app;
--- so-pekoko/src/routes/auth.js
+++ so-pekoko/src/routes/auth.js
@@ -3,12 +3,12 @@ import controller from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/api/auth/users', controller.getUsers.bind(controller));
+router.get('/users', controller.getUsers.bind(controller));
 
-router.post('/api/auth/signup', controller.signup.bind(controller));
+router.post('/signup', controller.signup.bind(controller));
 
-router.post('/api/auth/login', controller.login.bind(controller));
+router.post('/login', controller.login.bind(controller));
 
-router.delete('/api/auth/delete', controller.deleteUser.bind(controller));
+router.delete('/delete', controller.deleteUser.bind(controller));
 
 export default router;
