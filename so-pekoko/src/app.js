478b89b9e4a5856ccdb60dcfbe4b8e44b80d4a60
benjamin <benjamin.zamour@gmail.com>
2021-07-05 23:56:16 +0200

    app.js: Add url encoded data support + static image serving

--- so-pekoko/src/app.js
+++ so-pekoko/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import auth from './routes/auth.js';
 import sauces from './routes/sauces.js';
@@ -7,8 +8,11 @@ import sauces from './routes/sauces.js';
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded());
 app.use(express.json());
 app.use('/api/auth', auth);
 app.use('/api/sauces', sauces);
+
+app.use('/images', express.static(path.join(path.dirname(''), 'images')));
 
 export default app;
