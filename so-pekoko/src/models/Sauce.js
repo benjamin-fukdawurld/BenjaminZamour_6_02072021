499f237344c2481e11c9716d5f7f4dd2276c8bf1
benjamin <benjamin.zamour@gmail.com>
2021-07-06 20:39:59 +0200

    models/Sauces.js: Add constraints to fields

--- so-pekoko/src/models/Sauce.js
+++ so-pekoko/src/models/Sauce.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const SauceSchema = new Schema({
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true, min: 0 },
+  name: { type: String, required: true, minLength: 1, trim: true },
+  manufacturer: { type: String, required: true, minLength: 1, trim: true },
+  description: { type: String, required: true, minLength: 1, trim: true },
+  mainPepper: { type: String, required: true, minLength: 1, trim: true },
+  imageUrl: { type: String, required: true, trim: true },
+  heat: { type: Number, required: true, min: 1, max: 10 },
   likes: { type: Number, default: 0, min: 0 },
   dislikes: { type: Number, default: 0, min: 0 },
   userId: {
