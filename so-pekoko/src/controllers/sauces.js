d7513f435686c047772b32ca4e70d94e2d0313e3
benjamin <benjamin.zamour@gmail.com>
2021-07-06 11:08:08 +0200

    controllers/sauces.js: Implemented updateSauce & deleteSauce

--- so-pekoko/src/controllers/sauces.js
+++ so-pekoko/src/controllers/sauces.js
@@ -1,6 +1,24 @@
 import Sauce from '../models/Sauce.js';
+import fs from 'fs/promises';
+import path from 'path';
 
 class Controller {
+  getImageUrl(req) {
+    return `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+  }
+
+  deleteImageIfExists(filename) {
+    const filePath = path.resolve(path.dirname(''), 'images', filename);
+    return new Promise((resolve, reject) => {
+        fs.access(filePath)
+        .then(() => {
+          fs.unlink(filePath)
+          resolve();
+        })
+        .catch(() => resolve());
+    });
+  }
+
   getSauces(req, res) {
     Sauce.find()
       .then((sauces) => {
@@ -27,7 +45,7 @@ class Controller {
     delete sauceData._id;
     const sauce = new Sauce({
       ...sauceData,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      imageUrl: this.getImageUrl(req)
     });
 
 
@@ -44,17 +62,60 @@ class Controller {
   updateSauce(req, res) {
     if(!req.params.id) {
       res.status(400).send({message: 'Missing information'});
+      return;
     }
 
-    Sauce.findOne({_id: req.params.id}).then(result => {
+    const sauceData = req.file ? {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: this.getImageUrl(req)
+    } : { ...req.body };
 
+    Sauce.findOne({_id: req.params.id}).then(result => {
+      if(!result) {
+        res.status(404).send({ message: 'Cannot find sauce'});
+        return;
+      }
+
+      if(!req.user.priviledge && result.userId.toString() !== req.user.userId) {
+        res.status(403).send({ message: 'Operation unauthorized'});
+        return
+      }
+
+      const filename = result.imageUrl.split('/images/')[1];
+      this.deleteImageIfExists(filename).then(() => {
+        Sauce.updateOne({ _id: req.params.id }, { _id: req.params.id, ...sauceData })
+          .then(() => res.status(200).send({ message: 'Sauce updated' }))
+        });
     }).catch(error => {
       res.status(400).send(error);
     })
   }
 
   deleteSauce(req, res) {
-    res.status(500).send({});
+    if(!req.params.id) {
+      res.status(400).send({message: 'Missing information'});
+      return;
+    }
+
+    Sauce.findOne({_id: req.params.id}).then(result => {
+      if(!result) {
+        res.status(404).send({ message: 'Cannot find sauce'});
+        return;
+      }
+
+      if(!req.user.priviledge && result.userId.toString() !== req.user.userId) {
+        res.status(403).send({ message: 'Operation unauthorized'});
+        return
+      }
+
+      const filename = result.imageUrl.split('/images/')[1];
+      this.deleteImageIfExists(filename).then(() => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).send({ message: 'Sauce removed' }))
+        });
+    }).catch(error => {
+      res.status(400).send(error);
+    })
   }
 
   likeSauce(req, res) {
