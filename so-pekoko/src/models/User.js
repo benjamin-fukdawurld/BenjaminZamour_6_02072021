08a07384967c402738b01621f0a26e9e09633639
benjamin <benjamin.zamour@gmail.com>
2021-07-06 11:01:37 +0200

    models/User.js: Add privilegde

--- so-pekoko/src/models/User.js
+++ so-pekoko/src/models/User.js
@@ -12,6 +12,7 @@ const UserSchema = new Schema({
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   },
   password: { type: String, required: true },
+  priviledge: { type: Number, default: 0 }
 });
 
 UserSchema.virtual('userId').get(function () {
