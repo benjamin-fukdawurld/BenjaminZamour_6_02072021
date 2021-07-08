3e12767a1bca50becdcd8df1edf22dfbce9c3968
benjamin <benjamin.zamour@gmail.com>
2021-07-08 13:16:46 +0200

    Add helmet to secure requests

--- so-pekoko/src/app.js
+++ so-pekoko/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import helmet from 'helmet';
 
 import auth from './routes/auth.js';
 import sauces from './routes/sauces.js';
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(helmet())
 app.use('/api/auth', auth);
 app.use('/api/sauces', sauces);
 
