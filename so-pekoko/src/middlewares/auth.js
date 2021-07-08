e711c73cb679d4b49cc0c8b0a6aceb40efd95807
benjamin <benjamin.zamour@gmail.com>
2021-07-08 14:59:37 +0200

    middlewares/auth.js: Add priviledge to auth to support admin routes

--- so-pekoko/src/middlewares/auth.js
+++ so-pekoko/src/middlewares/auth.js
@@ -1,17 +1,23 @@
 import jwt from 'jsonwebtoken';
 
-export default function(req, res, next) {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const { userId, priviledge } = jwt.verify(token, process.env.TOKEN_KEY);
-        if(req.body.userId && req.body.userId !== userId) {
-            throw { message: 'Invalid token' };
+export default function(requiredPriviledge) {
+    return function(req, res, next) {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const { userId, priviledge } = jwt.verify(token, process.env.TOKEN_KEY);
+            if(req.body.userId && req.body.userId !== userId) {
+                throw { message: 'Invalid token' };
+            }
+
+            if(requiredPriviledge && (!priviledge || requiredPriviledge > priviledge)) {
+                throw { message: 'Unautorized operation' };
+            }
+
+            req.user = { userId, priviledge };
+
+            next();
+        } catch(error) {
+            res.status(401).send(error);
         }
-
-        req.user = { userId, priviledge };
-
-        next();
-    } catch(error) {
-        res.status(401).send(error);
     }
 }
